ee0170297226b5a227d28b08556446e3edd8099e
jean-baptiste.van.parys <jean-baptiste.van.parys@student.howest.be>
2023-04-16 20:47:08 +0200

    init

---
+++ app/src/main/java/com/example/gentevent/MainScreen.kt
@@ -0,0 +1,11 @@
+package com.example.gentevent
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainScreen (modifier: Modifier = Modifier) {
+
+
+
+}
---
+++ app/src/main/java/com/example/gentevent/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.example.gentevent.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val GenteventBlue = Color(0xFF003696)
+val Genteventred = Color(0xFFE63946)
