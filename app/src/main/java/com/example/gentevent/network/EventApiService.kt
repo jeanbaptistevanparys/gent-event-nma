0dbbcbcf870db986276e86c502da16dcb2535d77
Jean-Baptiste Van Parys <jean-baptiste.van.parys@student.howest.be>
2023-06-10 00:30:54 +0200

    api fix

--- app/src/main/java/com/example/gentevent/network/EventApiService.kt
+++ app/src/main/java/com/example/gentevent/network/EventApiService.kt
@@ -1,13 +1,12 @@
 package com.example.gentevent.network
 
+import com.example.gentevent.BuildConfig
 import com.example.gentevent.model.Events
 import retrofit2.http.GET
 import retrofit2.http.Headers
 
 interface EventApiService {
-    @Headers("x-hasura-admin-secret: zhYR6GbBEHM3WwGCK2KeLh6rM4ROFH37Qv0IZxV6xWOXCKWdde3C4HrYnOVwcQi2")
+    @Headers(BuildConfig.API_KEY)
     @GET("events")
     suspend fun getEvents(): Events
-
-    //normaly the api key must be hidden in a .env file or something like that
 }
